ef765eee2ea8aeb18e011a04556a0bea53f2a660
odetireddyismail <106577334+odetireddyismail@users.noreply.github.com>
2025-02-04 22:10:21 +0530

    Paypal

---
+++ paypal/src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import Payments from './Com/Payments';
+
+function App() {
+  return (
+    <div className="App">
+      <Payments/>  
+    </div>
+  );
+}
+
+export default App;
